e25e0d3d8064e924897675811de3d262027cdb2a
Jander Laffita <janderdeveloper@gmail.com>
2026-05-06 06:48:51 -0400

    fix repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://DevJ-Software.github.io',
-  base: '/business_automatization',
+  base: '/promotion_compay_web',
 });
